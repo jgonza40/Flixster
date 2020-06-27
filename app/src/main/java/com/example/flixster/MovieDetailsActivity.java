d3428d8a01a4061daefada6219f76f61e4115dd7
jgonza40 <jessica_gonzalez1@brown.edu>
2020-06-27 09:32:20 -0700

    updating walkthrough and adding poster to second sctivity

--- app/src/main/java/com/example/flixster/MovieDetailsActivity.java
+++ app/src/main/java/com/example/flixster/MovieDetailsActivity.java
@@ -1,12 +1,17 @@
 package com.example.flixster;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.target.Target;
 import com.example.flixster.R;
 import com.example.flixster.models.Movie;
 import org.parceler.Parcels;
@@ -16,9 +21,12 @@ public class MovieDetailsActivity extends AppCompatActivity {
     // the movie to display
     Movie movie;
 
+    ImageView ivPoster;
     TextView tvTitle;
     TextView tvOverview;
     RatingBar rbVoteAverage;
+
+    //Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +36,17 @@ public class MovieDetailsActivity extends AppCompatActivity {
         tvTitle = (TextView) findViewById(R.id.tvTitle);
         tvOverview = (TextView) findViewById(R.id.tvOverview);
         rbVoteAverage = (RatingBar) findViewById(R.id.rbVoteAverage);
+        ivPoster = (ImageView) findViewById(R.id.ivPoster);
 
         // unwrap the movie passed in via intent, using its simple name as a key
         movie = (Movie) Parcels.unwrap(getIntent().getParcelableExtra(Movie.class.getSimpleName()));
         Log.d("MovieDetailsActivity", String.format("Showing details for '%s'", movie.getTitle()));
 
         // set the title and overview
+        String imgURL;
+        int radius = 30;
+        imgURL = movie.getPosterPath();
+        Glide.with(getApplicationContext()).load(imgURL).transform(new RoundedCorners(radius)).override(Target.SIZE_ORIGINAL, Target.SIZE_ORIGINAL).placeholder(R.mipmap.placeholder_foreground).into(ivPoster);
         tvTitle.setText(movie.getTitle());
         tvOverview.setText(movie.getOverview());
 
